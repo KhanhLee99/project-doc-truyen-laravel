c91f31c9af9bbb101e46c12b857575604dc7cc5a
KhanhLe <levietkhanh99@gmail.com>
2020-11-05 15:43:59 +0700

    protected table name

--- app/Category_Story.php
+++ app/Category_Story.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category_Story extends Model
 {
     //
+    protected $table = 'category_story';
     protected $fillable = [
         'story_id', 'category_id'
     ];
